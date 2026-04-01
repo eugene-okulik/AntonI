9b2008919ca08acd03b8841e69d7632940f4a592
Anton_Ibragimov <antonibragimov@yandex.ru>
2026-04-01 17:44:39 +0500

    Added new files task1 - task4

---
+++ homework/anton_ibragimov/Lesson_3/task3.py
@@ -0,0 +1,11 @@
+# Среднее арифметическое и геометрическое
+
+num1 = 8
+num2 = 18
+
+arithmetic_mean = (num1 + num2) / 2
+
+geometric_mean = (num1 * num2) ** 0.5
+
+print("Среднее арифметическое:", arithmetic_mean)
+print("Среднее геометрическое:", geometric_mean)
