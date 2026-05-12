9df78281b4cfe8247b3b83bc6a8527b4bfb330b0
Anton_Ibragimov <antonibragimov@yandex.ru>
2026-05-12 15:52:48 +0500

    Fix lint

--- homework/anton_ibragimov/Lesson_5/basics_formatting.py
+++ homework/anton_ibragimov/Lesson_5/basics_formatting.py
@@ -3,7 +3,7 @@ person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(f"Name: {name}, Last Name: {last_name}, City: {city}, Phone: {phone}, Country: {country}")
 
-print("-" * 30) # Разделитель для красоты
+print("-" * 30)
 
 # Задание 2: Срезы и индексы
 # Строка 1
